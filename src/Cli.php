5ad159052a225815a16468399c578cd1393a62af
vladislav <vladislav.frint@gmail.com>
2023-06-16 14:04:23 +0300

    add src dir

---
+++ src/Cli.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BrainGames\Cli;
+
+// Add autoload file to use all available functions
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Function to act with cli
+use function cli\line;
+use function cli\prompt;
+
+
+/**
+ * Function to greet user
+ */
+function greeting() : void {
+  line('Welcome to the Brain Game!');
+  $name = prompt('May I have your name?');
+  line("Hello, %s!", $name);
+}
